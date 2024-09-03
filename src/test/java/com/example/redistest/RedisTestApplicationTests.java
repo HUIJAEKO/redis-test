8b6fbd59f9869d11225b98ff0992b1b613215805
HUIJAEKO <xrhgmlwox@naver.com>
2024-09-03 12:17:25 +0900

    connect redis

--- src/test/java/com/example/redistest/RedisTestApplicationTests.java
+++ src/test/java/com/example/redistest/RedisTestApplicationTests.java
@@ -1,6 +1,7 @@
 package com.example.redistest;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -9,5 +10,18 @@ class RedisTestApplicationTests {
     @Test
     void contextLoads() {
     }
+
+    @Autowired
+    private PersonRepository personRepository;
+
+    @Test
+    void test(){
+        Person person = new Person("KO", 25);
+
+        personRepository.save(person);
+
+    }
+
+
 
 }
